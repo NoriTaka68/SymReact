6a9dd5dee402f6e902ba48bba2c273817dda3633
NoriTaka68 <said@evhr.net>
2022-02-04 00:37:45 +0100

    Add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
+use App\Entity\Invoice;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +14,30 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        for ($c=0; $c < 30 ; $c++) { 
+            
+            $customer = new Customer();
+            $customer->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName())
+                    ->setCompany($faker->company())
+                    ->setEmail($faker->email());
+            
+            $manager->persist($customer);
+
+            for ($i=0; $i < mt_rand(3, 10); $i++) { 
+                
+                $invoice = new Invoice();
+                $invoice->setAmount($faker->randomFloat(2, 250, 5000))
+                        ->setSentAt($faker->dateTimeBetween('-6 months'))
+                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+                        ->setCustomer($customer);
+
+            $manager->persist($invoice);
+            }
+        }
 
         $manager->flush();
     }
